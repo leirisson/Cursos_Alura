549384f06d8e6a8329f574f4df2d670eabafb26c
leirisson <103783610+leirisson@users.noreply.github.com>
2023-06-21 23:36:19 -0400

    sfvdv

---
+++ Python/CadastrarAlunos/cadastrar aluno com nota e turma/CadastroAluno.py
@@ -0,0 +1,46 @@
+import os
+def ExibirMenu():
+    Titulo = "Sistema de cadastro de aluno"
+    print("#"*len(Titulo))
+    print(f"{Titulo}")
+    print("1 - Cadastrar")
+    print("2 - Listar Cadastro")
+    print("0 - Para sair")
+    print("#"*len(Titulo))
+
+while True:
+    ExibirMenu()
+    op = int(input("Qual dua opção: "))
+    if (op == 0):
+        break
+    elif(op == 1):
+        Cadastro_Aluno = 0
+        while True:
+            Cadasdro_aluno = {"nome":"", "idade":"", "curso":""}
+            datanascimento = str(input("Qual a data de nascimendo do aluno ex Dia-Mes-Ano: "))
+
+            nome = str(input("Digite seu nome Completo: ")).strip()
+            caminho = f"Cadastros/{datanascimento}-{nome}.txt"
+
+            with open(f'{caminho}', "w") as arquivo:
+                Cadasdro_aluno['nome'] = nome
+                Cadasdro_aluno['idade'] = int(input("Qual a idade do aluno: "))
+                Cadasdro_aluno['curso'] = str(input("Qual o curso do aluno: "))
+                arquivo.write(f"Nome: {Cadasdro_aluno['nome']}\nIdade: {Cadasdro_aluno['idade']}\nCurso: {Cadasdro_aluno['curso']}")
+                op = str(input("Deseja continuar: [S/N]"))
+                Cadasdro_aluno +=1
+                if(op in 'Ss'):
+                    continue
+                else:
+                    break
+
+    elif op == 2:
+        caminho = "Cadastros/"
+        pasta_arquivos = os.listdir(caminho)
+
+        for cadastros in pasta_arquivos:
+            print(cadastros)
+
+
+
+
--- Python/CadastrarAlunos/main.py
+++ Python/CadastrarAlunos/main.py
@@ -1,1 +1,9 @@
-print("oi")
+gravar_nome = open("gravar_nome.txt","a")
+nome = str(input("Qual seu nome: "))
+gravar_nome.write(f"\n{nome}")
+gravar_nome.close()
+
+
+with open("gravar_nome.txt","r") as arquivo:
+    conteu = arquivo.read()
+    print(conteu)
